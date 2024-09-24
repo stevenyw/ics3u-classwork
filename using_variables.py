d4a39d407a2e04ddcc9ceb1b4a8653254d979623
steven <steven.wong26@ycdsbk12.ca>
2024-09-23 21:26:54 -0400

    Add files via upload
    did you know pizza is great but 9/11 was terrible

---
+++ using_variables.py
@@ -0,0 +1,6 @@
+room_number = "113"
+number_thing = "2.71828"
+subject_name = "Computer Science"
+print("This is room #" + str(room_number) + ".")
+print(f"e is close to {number_thing}.")
+print("I am learning a little bit about {}.".format(subject_name))
