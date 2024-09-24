0378aa427e1ff80f84bf15ed5033b7fdab60090a
steven <steven.wong26@ycdsbk12.ca>
2024-09-24 11:02:25 -0400

    Add files via upload
    spongebob patrick sandy mr krabs squidward plankton patrick gary

---
+++ your_schedule (2).py
@@ -0,0 +1,28 @@
+course1 = "Math"
+teacher1 = "Mr. Leche"
+course2 = "Computer Science"
+teacher2 = "Mr. Gallo"
+course3 = "Engineering"
+teacher3 = "Mr. Burns"
+course4 = "Accounting"
+teacher4 = "Mr. Cheese"
+course5 = "English"
+teacher5 = "Mr. Shakespeare"
+course6 = "Debates & Arguments"
+teacher6 = "Mr. Trump"
+course7 = "Music"
+teacher7 = "Mr. Kanye West"
+course8 = "Technology Design"
+teacher8 = "Mr. Steve Jobs"
+liner = "-"
+
+print(f"{liner * 60}")
+print(f"{course1}                   |                   {teacher1}              |")
+print(f"{course2}       |                   {teacher2}              |")
+print(f"{course3}            |                   {teacher3}               |")
+print(f"{course4}             |                   {teacher4}              |")
+print(f"{course5}                |                   {teacher5}           |")
+print(f"{course6}    |                   {teacher6}             |")
+print(f"{course7}                  |                   {teacher7}         |")
+print(f"{course8}      |                   {teacher8}       |")
+print(f"{liner * 60}")                                                  
