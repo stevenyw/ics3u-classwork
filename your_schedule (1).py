cecbe91c86542a74f0d1839e81e9a0494b74024e
steven <steven.wong26@ycdsbk12.ca>
2024-09-24 20:40:39 -0400

    Add files via upload

---
+++ your_schedule (1).py
@@ -0,0 +1,28 @@
+course1 = "Math"
+teacher1 = "Mr. Leche"
+course2 = "Computer Science"
+teacher2 = "Mr. Gallo"
+course3 = "Engineering"
+teacher3 = "Mr. Burns"
+course4 = "Accounting"
+teacher4 = "Mr. Cheese"
+course5 = "English"
+teacher5 = "Mr. Shakespeare"
+course6 = "Debates & Arguments"
+teacher6 = "Mr. Trump"
+course7 = "Music"
+teacher7 = "Mr. Kanye West"
+course8 = "Technology Design"
+teacher8 = "Mr. Steve Jobs"
+liner = "-"
+
+print(f"{liner * 80}")
+print(f"|1|         {course1}                   |                   {teacher1}              |")
+print(f"|2|         {course2}       |                   {teacher2}              |")
+print(f"|3|         {course3}            |                   {teacher3}              |")
+print(f"|4|         {course4}             |                   {teacher4}             |")
+print(f"|5|         {course5}                |                   {teacher5}        |")
+print(f"|6|         {course6}    |                   {teacher6}              |")
+print(f"|7|         {course7}                  |                   {teacher7}         |")
+print(f"|8|         {course8}      |                   {teacher8}         |")
+print(f"{liner * 80}")                                                  
