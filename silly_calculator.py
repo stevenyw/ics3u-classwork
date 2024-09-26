b5cad65f9b48d74bf37890df701611935f348f2f
steven <steven.wong26@ycdsbk12.ca>
2024-09-25 21:22:20 -0400

    Add files via upload
    did you know that the tooth fairy doesnt exist

---
+++ silly_calculator.py
@@ -0,0 +1,7 @@
+num1 = float(input("What's your first number? "))
+num2 = float(input("What's your second number? "))
+num3 = float(input("What's your last number? "))
+
+total = (num1 + num2 + num3)/2
+
+print(f"{num1} + {num2} + {num3} / 2 is... {total}. That's pretty cringe aint it bruv.")
