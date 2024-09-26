dd99b7048276b500dad4a289e2106d7f0b150a1e
steven <steven.wong26@ycdsbk12.ca>
2024-09-25 20:52:41 -0400

    Add files via upload

---
+++ name_age_salary.py
@@ -0,0 +1,4 @@
+name = input("Hi! What's your name?")
+age = input(f"Hi {name}! Nice to meet you! :] How old are you?")
+salary = input(f"Dang brother, I didn't know you were {age}! How much money do you make? No shame if it's minnimum wage by the way.")
+print(f"{salary}! Dang! That's more than what I make :[")
