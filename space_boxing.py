985bfefc0993d7f076989025a60e957887ee1ec6
steven <steven.wong26@ycdsbk12.ca>
2024-10-01 11:02:15 -0400

    Add files via upload
    pen

---
+++ space_boxing.py
@@ -0,0 +1,11 @@
+weight = int(input("Please enter your earth weight."))
+
+print("I have info on these planets.")
+print("1	Venus	0.78")
+print("2	Mars	0.39")
+print("3	Jupiter	2.65")
+print("4	Saturn	1.17")
+print("5	Uranus	1.05")
+print("6	Neptune	1.23")
+visit = int(input("Choose which planet in number form you want to visit."))
+
