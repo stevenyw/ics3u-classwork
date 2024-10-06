18573b06f00b651eb80d21560afbe0b8d0c4507e
steven <steven.wong26@ycdsbk12.ca>
2024-10-06 00:33:51 -0400

    Update how_old_specifically.py

--- ics-classwork/how_old_specifically.py
+++ ics-classwork/how_old_specifically.py
@@ -3,9 +3,9 @@ age = int(input(f"Wow, what a GREAT name {name}! I'm kidding. Just give me your 
 
 if age < 16:
     print(f"Haha {name}! You can't drive! What are you, a fetus?")
-elif age >= 16 and age < 18:
+elif 16 <= age < 18:
     print(f"Cool {name}! You can drive but you can't vote! What are you, a teenager?")
-elif age >= 18 and age < 21:
+elif 18 <= age < 21:
     print(f"Cool {name}! You can drive & vote, but you can't rent a car! What are you, a young adult?")
 else:
     print("Oh you can do anything basically. What are you, an adult?")
