8ad108d6118c3f8abb2ed80e64c306d7815a899d
steven <steven.wong26@ycdsbk12.ca>
2024-10-07 00:34:59 -0400

    Update if_intro.py

--- ics-classwork/if_intro.py
+++ ics-classwork/if_intro.py
@@ -1,6 +1,10 @@
-robot_location = 30
-ball_location = 35
-goal_location = 20
+# The if statements will change the code underneath it by applying the changes from the code on top of it. If its false, it'll skip the if statement.
+# So the code doesn't mistake it for just printing it out by itself, and it'll only print it out if the if statement says the code is true.
+# The += means num + 1, or take the number and add it by 1, and the -= means to take the number and minus it by one.
+
+robot_location = 25
+ball_location = 30
+goal_location = 10
 have_ball = False
 
 if robot_location < ball_location:
