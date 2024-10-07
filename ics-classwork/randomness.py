4ef6d19b165831b31433a24f00da78a9e2a36c70
steven <steven.wong26@ycdsbk12.ca>
2024-10-06 22:35:01 -0400

    Add files via upload
    today i forgot to eat anything and it was like 6pm and both of my parents were working so i just made myself nutella bread and a banana and went back up to my room. nutrition amirite?

---
+++ ics-classwork/randomness.py
@@ -0,0 +1,45 @@
+# The new range will be 1 to 4.
+# The numbers become exactly the same every time.
+# The numbers became different but they still display the same numbers every time.
+# Minecraft uses "seed" programs to generate a new type of world every time. They use it to make different worlds for every new game you play.
+
+import random
+random.seed(300)
+
+
+x = random.randrange(10)  # 0-9
+print(f"My random number is {x}.")
+
+print()
+print("Here are some random numbers from 1 to 5...")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6), end=", ")
+print(random.randrange(1, 6))
+
+print()
+print("Here are some random numbers from 1 to 100...")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101), end=", ")
+print(random.randrange(1, 101))
+
+print()
+print("Will these next two random number be the same?")
+a = random.randrange(10)  # 0-9
+b = random.randrange(10)
+
+if a == b:
+    print(f"Wow! Both numbers were {a}!")
+else:
+    print("The two random numbers were different. Not too surprising.")
