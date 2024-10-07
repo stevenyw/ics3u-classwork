76d25cc5ca2987d361de27795b9fc9ea03547b0a
steven <steven.wong26@ycdsbk12.ca>
2024-10-07 00:02:11 -0400

    Add files via upload

---
+++ ics-classwork/fortune_cookie.py
@@ -0,0 +1,24 @@
+import random
+number = random.randrange(1, 7)
+if number == 1:
+    print("Today you will surpass all others.")
+elif number == 2:
+    print("Today you will get a girlfriend. Online. In reality it'll probably be some 40-year-old fat neckbeard.")
+elif number == 3:
+    print("Today you will die. How? Idk! Good luck! >w<")
+elif number == 4:
+    print("Today you will disappoint your parents. As usual.")
+elif number == 5:
+    print("You'll meet P. Diddy today and go to his mansion to have some fun with his baby oil collection.")
+elif number == 6:
+    print("You'll get assassinated Lincoln-style in the next week. Have fun! :D")
+else:
+    print("I'm a robot-fortune cookie, not an infinite cookie, idiot.")
+    
+print(random.randrange(1, 55), end=(" - "))
+print(random.randrange(1, 55), end=(" - "))
+print(random.randrange(1, 55), end=(" - "))
+print(random.randrange(1, 55), end=(" - "))
+print(random.randrange(1, 55), end=(" - "))
+print(random.randrange(1, 55))
+
