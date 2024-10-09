b5b68797020107334dd6dcd83a21b965e5bf73de
steven <steven.wong26@ycdsbk12.ca>
2024-10-09 10:14:49 -0400

    Update and rename countingotherexercise to while_loop_practice.py
    i nate hickers amirite my sigmas

---
+++ ics-classwork/while_loop_practice.py
@@ -0,0 +1,50 @@
+n = 0
+while n <10:
+    print(n)
+    n += 1
+
+o = 10
+while o >= 0:
+    print(o)
+    o -= 1
+
+k = 491
+while k < 591:
+    print(k)
+    k += 5
+
+a = 124
+while a < 810:
+    print(a+14)
+    a += 14
+
+b = -233
+while b <= -113:
+    print(b)
+    b += 4
+
+c = 215
+while c <= 300:
+    print(c)
+    c += 5
+
+d = -310
+while d <= -94:
+    print(d)
+    d += 8
+
+e = -476
+while e <= -410:
+    print(e)
+    e += 3
+
+f = -327
+while f <= -147:
+    print(f)
+    f += 9
+
+g = 484
+while g <= 1024:
+    print(g)
+    g += 18
+
