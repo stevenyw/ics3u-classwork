e55d4af3913f99ed2b11247107f85bbf8dec4749
steven <steven.wong26@ycdsbk12.ca>
2024-10-09 11:05:11 -0400

    Create ics23-accumulator-variable-practice.py

---
+++ ics-classwork/ics23-accumulator-variable-practice.py
@@ -0,0 +1,64 @@
+a = 1
+sum = 0
+while a <= 10:
+    print(a)
+    sum += a
+    a += 1
+    
+print(sum)
+    
+b = 100
+sum2 = 0
+while b <= 200:
+    print(b)
+    sum2 += b
+    b += 1
+    
+print(sum2)
+
+b = 100
+sum2 = 0
+while b <= 200:
+    print(b)
+    sum2 += b
+    b += 1
+    
+print(sum2)
+
+c = 100
+sum3 = 0
+while c <= 200:
+    print(c)
+    sum3 += c
+    c += 1
+d = 200
+sum4 = 0
+while d <= 300:
+    print(d)
+    sum4 += d
+    d += 1
+    
+print(sum3-sum4)
+
+e = 100
+multiple = 0
+while e <= 10000:
+    print(e)
+    multiple += e
+    e += 5
+print(multiple)
+    
+  f = 100
+multiple = 0
+tmultiple = 0
+while f <= 200:
+    print(f)
+    multiple += f
+    f += 5
+    tmultiple = % multiple
+print(multiple)
+    
+    
+
+    
+
