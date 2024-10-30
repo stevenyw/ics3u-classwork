3e63ad259890770f5b1635610c05716e13f8d926
steven <steven.wong26@ycdsbk12.ca>
2024-10-30 01:37:34 -0400

    Add files via upload

---
+++ ics-classwork/list0.py
@@ -0,0 +1,13 @@
+the_list = []
+the_list.append('-113')
+
+print(f"Slot 0 contains a {the_list}")
+print(f"Slot 1 contains a {the_list}")
+print(f"Slot 2 contains a {the_list}")
+print(f"Slot 3 contains a {the_list}")
+print(f"Slot 4 contains a {the_list}")
+print(f"Slot 5 contains a {the_list}")
+print(f"Slot 6 contains a {the_list}")
+print(f"Slot 7 contains a {the_list}")
+print(f"Slot 8 contains a {the_list}")
+print(f"Slot 9 contains a {the_list}")
