d7abe5f5090813a378f7b1378ef506f409b521b4
steven <steven.wong26@ycdsbk12.ca>
2024-10-30 02:04:45 -0400

    Update list1.py

--- list1.py
+++ list1.py
@@ -1,9 +1,6 @@
 the_list = []
-looper = the_list.append('-113')
-for list in range(0, 9):
+for list in range(0, 10):
     the_list.append('-113')
     
 for n in range(len(the_list)):
     print(f"Slot {n} contains a {the_list[n]}")
-    
-
