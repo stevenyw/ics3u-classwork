19b9ac504afcae1127e2e6c2d6514a1780302f7d
steven <steven.wong26@ycdsbk12.ca>
2024-10-31 11:05:14 -0400

    Update house.py

--- ics-classwork/house.py
+++ ics-classwork/house.py
@@ -14,12 +14,14 @@ clock = pygame.time.Clock()
 # Initialize global variables
 face_x = 100
 face_y = 1
-sphere_x = 110
-sphere_y = 10
 speed = 5
 radius = 110
+sun_x = 100
+sun_y = 100
 points = ((250, 200), (400, 50), (550, 200))
-circlecolor = ((255, 255, 0))
+suncolor = ((224, 217, 0))
+suncolor2 = ((191, 185, 2))
+suncolor3 = ((224, 217, 0))
 bgcolor = ((80, 131, 197))
 # ---------------------------
 
@@ -29,19 +31,29 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
 
-    sphere_y += speed
-    if sphere_y > HEIGHT + radius:
-        circlecolor = ((64, 64, 64))
+    sun_y += speed
+
+    if sun_y > HEIGHT + radius and sun_y > HEIGHT + radius:
+        suncolor = ((64, 64, 64))
+        suncolor2 = ((77, 77, 77))
+        suncolor3 = ((0,0,0))
         bgcolor = ((0, 0, 0))
         speed *= -1
-    if sphere_y < 0 - radius:
-        circlecolor = ((255, 255, 0))
+
+    if sun_y < 0 - radius and sun_y < 0 - radius:
+        suncolor = ((184, 156, 17))
+        suncolor2 = ((191, 185, 2))
+        suncolor3 = ((224, 217, 0))
         bgcolor = ((80, 131, 197))
         speed *= -1
+
+    
     
         
 
@@ -51,8 +63,14 @@ while running:
     pygame.draw.rect(screen, (198, 188, 164), (face_x+200, face_y+200, face_x+100, face_y+200))
     pygame.draw.rect(screen, (198, 188, 164), (face_x+350, face_y + 50, face_x-50, face_y+100))
     pygame.draw.polygon(screen, (168, 45, 42), ((face_x+150, face_y+199), (face_x+300, face_y+49), (face_x+450, face_y+199)))
-    pygame.draw.circle(screen, circlecolor, (face_x+10, sphere_y), face_x-80)  # sun
-    pygame.draw.rect(screen, (30, 97, 66), (face_x-100, face_y+400, face_x+2000, face_y+200))  #ground
+    pygame.draw.line(screen, suncolor3, (80, sun_y+27), (135, sun_y-31), 7)
+    pygame.draw.line(screen, suncolor3, (134, sun_y+27), (85, sun_y-31), 7)
+    pygame.draw.rect(screen, (suncolor3), (sun_x-28, sun_y-2, 75, 5))
+    pygame.draw.rect(screen, (suncolor3), (sun_x+7.75, sun_y-35, 5, 75))
+    pygame.draw.circle(screen, (suncolor), (sun_x+10, sun_y), 20)
+    pygame.draw.circle(screen, (suncolor2), (sun_x+10, sun_y), 15)  # sun
+
+    pygame.draw.rect(screen, (1, 102, 31), (face_x-100, face_y+400, face_x+2000, face_y+200))  #ground
     for x in range(50, 101, 25):
         pygame.draw.rect(screen, (198, 188, 164), (face_x+200, face_y+199, face_x+100, face_y+200))
     # Must be the last two lines
