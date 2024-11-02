6b0fdcece04e11608cd2587778851a275a691738
steven <steven.wong26@ycdsbk12.ca>
2024-11-02 01:35:53 -0400

    Update list3.py

--- ics-classwork/list3.py
+++ ics-classwork/list3.py
@@ -2,7 +2,7 @@ import random
 
 the_list = []
 for list in range(0, 1001):
-    the_list.append(random.randrange(10, 99))
+    the_list.append(random.randrange(10, 100))
 
     
 for n in range(len(the_list)):
