ca44e00dfc23b855e8fd1c742470fa66a28ccdb4
steven <steven.wong26@ycdsbk12.ca>
2024-11-04 00:37:27 -0500

    Add files via upload

---
+++ ics-classwork/how_many_times.py
@@ -0,0 +1,19 @@
+import random
+my_list = []
+for i in range(0, 10):
+    my_list.append(random.randrange(0, 51))
+
+print("list 1:")
+for m in range(len(my_list)):
+    print(f"{my_list[m]}", end=" ")
+
+
+search = int(input("Which number do you wanna search?"))
+e = 0
+for n in range(len(my_list)):
+    if search == my_list[n]:
+        e += 1
+print(f"{search} is on the list {e} times")
+    
+
+    
