23da7b1d834295c2b6af221111e7856f5a846d72
steven <steven.wong26@ycdsbk12.ca>
2024-11-04 11:04:59 -0500

    Add files via upload

---
+++ ics-classwork/split_and_convert.py
@@ -0,0 +1,53 @@
+pets = "monkey bat cat dog"
+
+for n in pets.split(" "):
+    print(n)
+    
+numbers = "65, 75, 32, 22"
+numbers2 = []
+for n in numbers.split(", "):
+    numbers2.append(int(n))
+
+for m in numbers2:
+    print(m)
+    
+mystr = "one,two,three,four"
+newstring = []
+for n in mystr.split(","):
+    newstring.append((n))
+newerstring = []
+for new in range(len(newstring)):
+    newerstring.append((new+1))
+print(newerstring)
+
+score = "W W L T T W"
+scorestring = []
+for n in score.split(" "):
+    if n == "W":
+        scorestring.append(2)
+    elif n == "L":
+        scorestring.append(0)
+    else:
+        scorestring.append(1)
+print(scorestring)
+
+values = "x:3,x:6,x:14,x:22"
+xval = []
+for n in values.split(","):
+    m = int(n.split(":")[1])
+    xval.append(m)
+print(xval)
+
+second_values = "x:2,y:5 - x:5,y:11 - x:7,y:14"
+nextvalues = []
+for n in second_values.split("x:"):
+    m = int(n.split(",y:")[1])
+    z = int(m.split(" - "))
+    nextvalues.append(z)
+print(nextvalues)
+
+    
+
+    
+
+    
